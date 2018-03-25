8fdfccde3df628e0187e6ccbfb95137cee34470a
mac <mac@mac>
2018-03-24 22:24:33 -0500

    Cleans IdentifyUserTest

--- library/user_session/UnitTests/IdentifyUserTest.php
+++ library/user_session/UnitTests/IdentifyUserTest.php
@@ -15,6 +15,12 @@ class IdentifyUserTest extends TestCase
 
     use UtilityTest;
 
+    protected $http;
+
+    protected $cookie;
+
+    protected $user;
+
     /**
      * Create test subject before test
      */
@@ -22,18 +28,30 @@ class IdentifyUserTest extends TestCase
     {
         parent::setUp();
 
+
+        // Mock user, simulates user retrieval from
+        // back end services
+        $this->user = new StdClass;
+        $this->user->name = "Some Name";
+
         $_SERVER['REMOTE_ADDR'] = "REMOTE_ADDR";
         $_SERVER['HTTP_USER_AGENT'] = "HTTP_USER_AGENT";
         $_SERVER['HTTP_ACCEPT'] = "HTTP_ACCEPT";
+
+        // Create a stub for the PhpHttpAdapter class
+        $this->http = $this->getMockBuilder(PhpHttpAdapter::class)
+            ->setMethods(array('setJsonData', 'getJsonData', 'send')) //tells mock builder which methods should be mocked
+            ->getMock();
+
+        // Create a stub for the CookieSetter Class class.
+        $this->cookie = $this->createMock(CookieSetter::class);
+
+        $this->cookie->method('gethash')
+            ->willReturn("mcsiljcincklsdncvklsdvisdn");
     }
 
     public function testIdentifyNewUser() {
         // Arrange
-
-        // Mock user, simulates user retrieval from
-        // back end services
-        $user = new StdClass;
-        $user->name = "Some Name";
 
         // Make sure $_SESSION and $_COOKIE are clean
         $_SESSION = array();
@@ -41,44 +59,29 @@ class IdentifyUserTest extends TestCase
 
         try {
 
-            // Create a stub for the PhpHttpAdapter class
-            $http = $this->getMockBuilder(PhpHttpAdapter::class)
-                ->setMethods(array('setJsonData', 'getJsonData', 'send')) // this line tells mock builder which methods should be mocked
-                ->getMock();
-
-            // Create a stub for the CookieSetter Class class.
-            $cookie = $this->createMock(CookieSetter::class);
-
-            $cookie->method('gethash')
-                ->willReturn("mcsiljcincklsdncvklsdvisdn");
-
-            // Mock rhe setCookie method that actually creates a cookie on the
+            // Mock the setCookie method that actually creates a cookie on the
             // users computer. Since this method is mocked, lets manually update
             // $_COOKIE super-global
-            $cookie->method('setCookie')
+            $this->cookie->method('setCookie')
             ->willReturnCallback(function (){$_COOKIE["some_website_user"] = "mcsiljcincklsdncvklsdvisdn";});
 
-
-            $cookie->method('getCookieName')
+            $this->cookie->method('getCookieName')
                 ->willReturn("some_website_user");
 
-            $http->method('getJsonData')
-                ->willReturn($user);
+            $this->http->method('getJsonData')
+                ->willReturn($this->user);
 
-            $identifyUser = IdentifyUser::IdentifyUser($cookie, $http);
+            $identifyUser = IdentifyUser::IdentifyUser($this->cookie, $this->http);
             $actualHash = $this->getProperty($identifyUser, "userHash");
             $actualUser = $this->getProperty($identifyUser, 'user');
 
             // Assert
             $this->assertSame("mcsiljcincklsdncvklsdvisdn", $actualHash, "Testing Expected Hash");
-            $this->assertSame($user, $actualUser, 'Testing Expected User');
-            $this->assertSame($_SESSION['some_website_user'], $user, "Testing proper setting of _SESSION");
+            $this->assertSame($this->user, $actualUser, 'Testing Expected User');
+            $this->assertSame($_SESSION['some_website_user'], $this->user, "Testing proper setting of _SESSION");
 
-            // Clean resources Testing only
-
+            // Clean resources / Singleton object
             $identifyUser->__destruct();
-            unset($identifyUser);
-            $identifyUser = null;
 
         } catch (Exception $e) {
             echo $e->getMessage();
@@ -88,44 +91,34 @@ class IdentifyUserTest extends TestCase
     public function testIdentifyExistingUser() {
         // Arrange
 
-        // Mock user, simulates user retrieval from
-        // back end services
-        $user = new StdClass;
-        $user->name = "Some Name";
-
         // Make sure $_SESSION is clean and $_COOKIE is set
         $_SESSION = array();
         $_COOKIE["some_website_user"] = "mcsiljcincklsdncvklsdvisdn";
 
         try {
 
-            // Create a stub for the PhpHttpAdapter class
-            $http = $this->getMockBuilder(PhpHttpAdapter::class)
-                ->setMethods(array('setJsonData', 'getJsonData', 'send')) // this line tells mock builder which methods should be mocked
-                ->getMock();
-
-            // Create a stub for the CookieSetter Class class.
-            $cookie = $this->createMock(CookieSetter::class);
-
-            $cookie->method('gethash')
+            $this->cookie->method('gethash')
                 ->willReturn("mcsiljcincklsdncvklsdvisdn");
 
 
-            $cookie->method('getCookieName')
+            $this->cookie->method('getCookieName')
                 ->willReturn("some_website_user");
 
-            $http->method('getJsonData')
-                ->willReturn($user);
+            $this->http->method('getJsonData')
+                ->willReturn($this->user);
 
             // Act
-            $identifyUser = IdentifyUser::IdentifyUser($cookie, $http);
+            $identifyUser = IdentifyUser::IdentifyUser($this->cookie, $this->http);
             $actualHash = $this->getProperty($identifyUser, "userHash");
             $actualUser = $this->getProperty($identifyUser, 'user');
 
             // Assert
             $this->assertSame("mcsiljcincklsdncvklsdvisdn", $actualHash, "Testing Expected Hash");
-            $this->assertEquals($user, $actualUser, 'Testing Expected User');
-            $this->assertSame($_SESSION['some_website_user'], $user, "Testing proper setting of _SESSION");
+            $this->assertEquals($this->user, $actualUser, 'Testing Expected User');
+            $this->assertSame($_SESSION['some_website_user'], $this->user, "Testing proper setting of _SESSION");
+
+            // Clean resources / Singleton object
+            $identifyUser->__destruct();
 
         } catch (Exception $e) {
             echo $e->getMessage();
